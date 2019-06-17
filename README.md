08aba6caf122cdfe5f78d8dd539e16483e312621
Pei-Lun Huang <peilun0912@gmail.com>
2019-06-17 17:14:14 +0800

    feat(receiver): initial version

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # NCDR Alert Receiver
+
+部署時，需要創個 `instance` 資料夾，在裡面新增 `config.py`，內容為：
+
+```Python
+# Required
+S3_BUCKET = 'S3 BUCKET'
+
+# Optional
+S3_PREFIX = 'S3 PREFIX
+```
---
+++ receiver.py
@@ -0,0 +1,44 @@
+import boto3
+from flask import Flask, request
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_pyfile('config.py')
+    s3_bucket = app.config['S3_BUCKET']
+    s3_prefix = app.config.get('S3_PREFIX')
+
+    @app.route('/')
+    def hello():
+        return 'Hello, World!'
+
+    @app.route('/webhook', methods=['POST'])
+    def receive():
+        filename = request.args.get('name')
+
+        if filename:
+            app.logger.info(f'Receiving {filename}')
+
+            s3_client = boto3.client('s3')
+            s3_key = f'{s3_prefix}{filename}'
+            s3_client.put_object(
+                Body=request.get_data(), Bucket=s3_bucket, Key=s3_key
+            )
+
+            app.logger.info(
+                f'{filename} has been uploaded to s3://{s3_bucket}/{s3_key}'
+            )
+
+            return (
+                '<?xml version="1.0" encoding="utf-8"?>'
+                '<Data><Status>True</Status></Data>',
+                {'Content-Type': 'text/xml'},
+            )
+
+    return app
+
+
+if __name__ == '__main__':
+    flask_app = create_app()
+    flask_app.run()
